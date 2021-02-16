ea5e01187320feca17645412b20a04f25c4f0b89
Brennen Berkley <bberkle@amazon.com>
2021-02-16 12:36:23 -0800

    Added more topics

---
+++ public/responses.js
@@ -0,0 +1,22 @@
+var responses = {
+  churchIsGood: {
+    body: "Even if the doctrine isn't true, the church still does a lot of good and is a positive force in the world",
+    rebuttal: 'churchIsHarmful'
+  },
+  churchIsCharitable: {
+    body: "The church donates X amount in humanitarian efforts every year",
+    rebuttal: 'churchIsNotCharitable'
+  },
+  ensignFundIsSmart: {
+    body: "The church is just being smart with their money and saving for a rainy day",
+    rebuttal: "ensignFund"
+  },
+  churchTeachesGoodValues: {
+    body: "The church teaches good values and is a good way to raise children",
+    rebuttal: "churchTeachesHarmfulValues"
+  },
+  churchHasStrongCommunity: {
+    body: "The church has a strong community",
+    rebuttal: "community"
+  }
+}
---
+++ public/topics.js
@@ -0,0 +1,39 @@
+var topics = {
+  bookOfMormon: {
+    title: 'The Book of mormon was plagiarized',
+    details: '[Insert evidence here]',
+    responses: [
+      'churchIsGood'
+    ]
+  },
+  churchIsHarmful: {
+    title: 'The LDS Church is harmful',
+    details: "LGBTQ+ issues, racism, sexism, personal shaming and guilt, modesty and self-worth issues, judgemental mindset, attitute that the second coming is near so earth doesn't matter",
+    responses: [
+      'churchIsCharitable',
+      'churchTeachesGoodValues',
+      'churchHasStrongCommunity'
+    ]
+  },
+  churchIsNotCharitable: {
+    title: 'Humanitarian contributions from the church',
+    details: "Humanitarian contributions from the church only make up about 1% of all that they receive from tithing, while the rest goes to building expensive temples and growing their 100B ensign fund. If you gave 10% to a charity it would be much more effective.",
+    responses: [
+      'ensignFundIsSmart'
+    ]
+  },
+  ensignFund: {
+    title: "Ensign fund",
+    details: "The church has $100B. If the church is saving for a rainy day, when is that day? Is the current pandemic not a good time to use that money to help people?",
+  },
+  churchTeachesHarmfulValues: {
+    title: 'Church does not teach good values to our kids',
+    details: 'The church definitely has some positives, such as their focus on music and education. But you can emphasize those things without the church as so many non-christian parents do. And the church also teaches lots of harmful values regarding self worth, self confidence, being judgemental, and others.',
+    responses: []
+  },
+  community: {
+    title: 'The church is not the only place where you can find a community',
+    details: "The church is a great way to meet people and make friends. But those friendships often aren't very strong anyway because your only common interest is the church. Friendships made through sports leagues or other meetups would be much stronger because you would have more common interests. And if you do choose to continue going to church for the community, at least stop paying tithing or pay it to an actual charity instead.",
+    responses: []
+  }
+}
